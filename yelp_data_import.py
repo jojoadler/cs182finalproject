05914bc6e3c5a16e8ad124d53884fef568344f95
Jojo Adler <jojoadler@Jojos-MacBook-Pro.local>
2018-12-14 23:34:54 +0200

    hello

--- yelp_data_import.py
+++ yelp_data_import.py
@@ -33,9 +33,25 @@ for restaurant in data[0:len(data)]:
 			restaurant_data.append(restaurant)
 
 #inputs for a sample problem
-user_inputs = {'days': [3], 'state': ['AZ'], 'city': ['Phoenix'], 'cuisines': [['Mexican', 1],['Italian', 2]], 'base_location': [(33.4484,-112.0740)], 'maxDist': [10]}
+user_inputs = {'days': [3], 'state': ['NC'], 'city': ['Belmont'], 'cuisines': [['Mexican', 1],['Italian', 2]], 'base_location': [(33.4484,-112.0740)], 'maxDist': [10]}
 # constraints for the sample problem, for now this is hardcoded, but should be automatically constructed
 constraints = [('Mexican', 'Italian'), ('Italian', 'Mexican')]
+
+belmont_businesses = [business for business in data if business['city'] == 'Belmont']
+
+belmont_cats = {}
+cats_split = []
+belmont_rests = []
+for rest in belmont_businesses:
+	if rest['categories']:
+		cats_split = [x.strip() for x in rest['categories'].split(',')]
+	if 'Restaurants' in cats_split:
+		belmont_rests.append(rest)
+		for cat in cats_split:
+			if cat in belmont_cats:
+				belmont_cats[cat] += 1
+			else:
+				belmont_cats[cat] = 1
 
 # create states
 states = {}
@@ -66,7 +82,7 @@ for restaurant in restaurant_data:
 			if rest_to_base <= user_inputs['maxDist']:
 				possible_restaurants.append(restaurant)
 
-state_domains = dict.fromkeys(copy.deepcopy(states), possible_restaurants)
+state_domains = dict.fromkeys(copy.deepcopy(states), possible_restaurants[:50])
 
 # creating arcs
 def constraint_generator(states):
@@ -75,45 +91,76 @@ def constraint_generator(states):
 	for state1 in states:
 		del states_not_checked1[state1]
 		for state2 in states_not_checked1:
-			constraints[(state1,state2)] = states[state1] != states[state2]
+			constraints[(state1,state2)] = (states[state1] != states[state2])
 	return constraints
 
-weirdlist = [(state1, state2) for state1 in states for state2 in neighbors[state1]]
+def check_sol (statess, bigX, littleX, bigY, littleY):
+	checkstates = copy.deepcopy(statess)
+	checkstates[bigX] = littleX
+	checkstates[bigY] = littleY
+	returnval = constraint_generator(checkstates).get((bigX,bigY), False)
+	return returnval
+
+def weirdlist (stts, neighbors): return [(statei, statek) for statei in stts for statek in neighbors[statei]]
 def AC3 (stts, domain, neighbs, queue = None):
 	if queue == None:
-		queue = weirdlist
+		queue = weirdlist(stts, neighbs)
 	while queue:
-		(state1, state2) = queue.pop()
-		if remove_arcs(stts, domain, neighbs, state1, state2):
-			if len(domain[state1]) ==0:
-				return False
-			for state3 in neighbors[state1]:
-				queue.append((state3, state1))
-	return queue
+		(statei, statej) = queue.pop()
+		if remove_arcs(stts, domain, neighbs, statei, statej):
+			"""if len(domain[statei]) == 0:
+				return False"""
+			for statek in neighbs[statei]:
+				queue.append((statek, statei))
+	#return queue
 
-def remove_arcs(stts, domain, neighbs, state1, state2):
+def remove_arcs(stts, domain, neighbs, statei, statej):
 	#figure out what the fuck [:] means and change it to your own syntax 
-	def consistent(state1, state2):
+	"""def consistent(state1, state2):
 		for poss_value in domain[state2]:
 			if state2 in neighbors[state1]: #& poss_val !=x:
 				return True
-		return False
+		return False"""
 
 	gone = False
 
-	for restaurant in domain[state1]:
-		if not consistent(state1, state2):
-			states[state1] = restaurant
+	for restaurantx in domain[statei]:
+		arc_checker = (map(lambda restauranty: not (restaurantx != restauranty), domain[statej]))
+		if arc_checker == [False for x in range(len(arc_checker))]:
+			#print not map(lambda restauranty: check_sol(stts, statei, restaurantx, statej, restauranty), domain[statej])
+			#print domain
+			domain[statei].remove(restaurantx)
 			gone = True
+	print gone
 
 	return gone
-	"""for state in domain[state1][:]:
-		if every(lambda y: not constraint_generator(states)[(state1,state2)], state_domains[state2]):
-			domain[state1].remove(state)
-			gone = True
-	return gone"""
 
-def min_conflicts(stts, domain, neighbs, maxx):
+practice_domains = {'a':[1,2,3,4],'b':[1,2,3,4],'c':[1,2,3,4],'d':[1,2,3,4]} 
+practice_states = {'a':None,'b':None,'c':None,'d':None} 
+practice_neighbors = {'a':['b','d'],'b':['a','c'],'c':['b','d',],'d':['a','c']}
+
+"""for state in domain[state1][:]:
+	if every(lambda y: not constraint_generator(states)[(state1,state2)], state_domains[state2]):
+		domain[state1].remove(state)
+		gone = True
+return gone"""
+
+"""def num_conflicts(states, domain, state):
+	counter = 0
+	relevant_constraints = {}
+	for key in states.keys():
+		s1, s2 = key
+		if s1 == state:
+			relevant_constraints[(s1,s2)] = copy.deepcopy(constraints[(s1,s2)])
+		if s2 == state:
+			relevant_constraints[(s1,s2)] = copy.deepcopy(constraints[(s1,s2)])
+
+	for constraint in relevant_constraints:
+		if constraint == False
+			counter += 1
+	return counter"""
+
+"""def min_conflicts(stts, domain, neighbs, maxx):
 	current = {} 
 	for state in states:
 		#should be least conflicts assignment but get to that later
@@ -124,7 +171,25 @@ def min_conflicts(stts, domain, neighbs, maxx):
 			if not conflicted:
 				return current
 			newState = random.choice(conflicted)
-			newRest = random.choice()
+			newRest = random.choice()"""
+
+"""def is_solution (states):
+	val = 0
+	for constraint in constraint_generator(states):
+		if constraint_generator(states) == False:
+			val += 1
+	if val == 0:
+		return True
+	else:
+		return False
+
+def backtracking (states, domains, forwardcheck = True):
+	if is_solution(states):
+		return states
+	else: """
+
+
+
 
 
 
