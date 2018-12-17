1754c714c353105f7ec4da2ea80976c0f82ce630
Jojo Adler <jojoadler@Jojos-MacBook-Pro.local>
2018-12-17 23:18:24 +0200

    try to add

---
+++ backtracking_yelp.py
@@ -0,0 +1,355 @@
+import tarfile
+import zipfile
+import json
+import geopy.distance
+import copy
+import random
+import math
+
+
+# CHANGE DIRECTORY HERE
+filepath = '/Users/jojoadler/Desktop/yelp_academic_dataset_business.json'
+#filepath = '/Users/amydanoff/Desktop/yelp_dataset/yelp_dataset/yelp_academic_dataset_business.json'
+
+"""****************************************************************************************
+	CODE FOR DATA CLEANING
+   ****************************************************************************************"""
+
+def open_data(filepath):
+	"""
+	Function that returns formatted data
+	"""
+	data = []
+	with open(filepath) as f:
+	    data = f.readlines()
+	    data = list(map(json.loads, data)) 
+	return data
+
+# List of all businesses in dataset
+data = open_data(filepath)
+
+#Will contain a list of dictionaries for restaurants only
+restaurant_data = []
+
+# sorts through data and extracts only those with category 'restaurants'
+for restaurant in data[0:len(data)]:
+	if restaurant['categories'] != None:
+		# turns unicode into strings
+		cats = ''.join([cat.encode('UTF8') for cat in restaurant['categories']])
+		catswords = cats.split(', ')
+		if 'Restaurants' in catswords:
+			restaurant_data.append(restaurant)
+
+def test_maker((loc, starweight, reviewweight, num_meals, constraints)):
+	import time
+	start = time.time()
+	def city_businesses(city): return [business for business in data if business['city'] == city]
+
+	"""belmont_businesses = city_businesses('Belmont')
+	phoenix_businesses = city_businesses('Phoenix')
+	parma_businesses = city_businesses('Parma')"""
+
+	"""belmont_cats = {}
+	cats_split = []
+	belmont_rests = []
+	for rest in belmont_businesses:
+		if rest['categories']:
+			cats_split = [x.strip() for x in rest['categories'].split(',')]
+		if 'Restaurants' in cats_split:
+			belmont_rests.append(rest)
+			for cat in cats_split:
+				if cat in belmont_cats:
+					belmont_cats[cat] += 1
+				else:
+					belmont_cats[cat] = 1"""
+
+	# create states
+	states = {}
+	# each state will be a tuple defined by the day and the meal (1,2) = sedond day dinner (indexed at 0)
+	for day in range(num_meals/3):
+		for meal in range(3):
+			states[(day,meal)] = None
+
+	# neighboring states
+	neighbors = dict.fromkeys(copy.deepcopy(states), [])
+	for state in states:
+		day, meal = state
+		for i in range(1,3):
+			if meal - i >= 0:
+				neighbors[state].append((day, meal - i))
+			if meal + i <= 2:
+				neighbors[state].append((day, meal + i))
+
+
+	# all possible values, ie domain
+	possible_restaurants = []
+	# check for every restaurant
+	"""for restaurant in restaurant_data:
+		# check for every restaurant in the location of the user
+		for loc in ['state', 'city']:
+			if restaurant[loc] == user_inputs[loc][0]:
+				rest_to_base = geopy.distance.vincenty(user_inputs['base_location'][0], (restaurant['latitude'], restaurant['longitude']))
+				if rest_to_base <= user_inputs['maxDist']:
+					possible_restaurants.append(restaurant)"""
+
+	state_domains = dict.fromkeys(copy.deepcopy(states), city_businesses(loc))
+
+	def strip_categories(categories):
+		"""
+		Converts a 'Categories' string into a list of categories
+		"""
+		categorieslist = []
+		for category in categories:
+			# turns unicode into strings
+			cats = (''.join(''.join([cat.encode('UTF8') for cat in category]))).split(', ')
+			categorieslist.append(catswords)
+		return categorieslist[1]
+
+	"""def category_list_maker(restaurants):
+		# Makes a dict of each restaurant, the list of categories they have
+		restaurant_categories = {}
+		for restaurant in restaurants:
+			restaurant_categories[(restaurant['business_id'])] = strip_categories(restaurant['categories'])
+		return restaurant_categories
+
+	def category_counter(restaurants):
+		categories = {}
+		for restaurant in restaurants:
+			for category in category_list_maker(restaurant)['business_id']:
+				if categories.get(category, None) == None:
+					categories[category] = 1
+				else:
+					categories[category] += 1
+		return categories
+
+	def user_solution_checker(user_dict, bigX, littleX, assignment, unique = False):
+		restaurant_categories = category_list_maker(assignment.values())
+		checker = copy.deepcopy(assignment)
+		if unique:
+			for category in user_dict.keys():
+				if category_counter(checker.values())[category] > 1:
+					return False
+		else:
+			for category in user_dict.keys():
+				if category_counter(checker.values())[category] > user_dict[category]:
+					return False
+		return True"""
+
+	def has_category(business, category):
+		"""
+		Takes in a business object and a category, returns True if the business has that category
+		and False otherwise
+		"""
+		categories = set()
+		if business['categories']:
+			categories = set(strip_categories(business['categories']))
+		if category in categories:
+			return True 
+		return False
+
+	def count_categories(businesses):
+		"""
+		Converts a list of business items into a dictionary of categories
+		Does not include count for 'Restaurants'
+		"""
+		categories = {}
+		for business in businesses:
+			cats_string = business['categories']
+			cats_list = []
+			if cats_string:
+				cats_list = strip_categories(cats_string)
+			for cat in cats_list:
+				if cat != 'Restaurants':
+					if cat in categories:
+						categories[cat] += 1
+					else:
+						categories[cat] = 1
+		return categories
+
+	def unique_categories(categories):
+		"""
+		Takes in a dictionary of categories and returns True if unique, else False
+		"""
+		return all(value < 3 for value in list(categories.values()))
+
+	def constraints_match(categories, constraints):
+		"""
+		Takes in a dictionary of category counts and a dictionary specifying the constraints, which are given as MAXIMUMS
+		and returns True if the constraints are satisfied, and False otherwise.
+		Any category that is not specified in the constraints dictionary can have any value.
+		Additionally, there is a category called 'Unique', whereby the user can specify that they want all unique categories.
+		"""
+
+		# Else, iterate through categories and ensure maximums are satisfied
+		for category, val in constraints.items():
+			if category != 'Unique':
+				# If category maximum is exceeded, return False
+				if val != 0:
+					if category in categories:
+						if categories[category] > val:
+							return False
+				else:
+					if category in categories:
+						return False
+
+		# If 'Unique' is specified, ensure categories are unique
+		if constraints['Unique']:
+			return unique_categories(categories)
+		return True
+
+
+	"""****************************************************************************************
+		CODE FOR AC-3
+	   ****************************************************************************************"""
+	def constraint_generator(states):
+		"""
+			Creates a dictionary of each arc and True is the constraint 
+			that two states cannot be equal is satisfied, False otherwise
+		"""	
+		constraints = {}
+		states_not_checked1 = copy.deepcopy(states)
+		for state1 in states:
+			del states_not_checked1[state1]
+			for state2 in states_not_checked1:
+				constraints[(state1,state2)] = (states[state1] != states[state2])
+		return constraints
+
+	def check_sol (statess, bigX, littleX, bigY, littleY):
+		"""
+			Will return True is any two hypothetical states satisfy constraints,
+			returns False otherwise
+		"""
+		checkstates = copy.deepcopy(statess)
+		checkstates[bigX] = littleX
+		checkstates[bigY] = littleY
+		returnval = constraint_generator(checkstates).get((bigX,bigY), False)
+		return returnval
+
+	def runAC3 (stts, domain, neighbs, queue = None):
+		def constraint (x, y): return (x != y)
+		def weirdlist (stts, neighbors): return [(statei, statek) for statei in stts for statek in neighbors[statei]]
+		def AC3 (stts, domain, neighbs, queue = None):
+			if queue == None:
+				queue = weirdlist(stts, neighbs)
+			while queue:
+				(statei, statej) = queue.pop()
+				if remove_arcs(stts, domain, neighbs, statei, statej):
+					"""if len(domain[statei]) == 0:
+						return False"""
+					for statek in neighbs[statei]:
+						queue.append((statek, statei))
+
+		def remove_arcs(stts, domain, neighbs, statei, statej):
+			"""
+				For each value in the domain of statei, remove any state from the domain of 
+				state i that conflicts with the constraints for every value in the domain of 
+				state j
+			"""
+			gone = False
+			for restaurantx in domain[statei]:
+				arc_checker = (map(lambda restauranty: not check_sol(states, statei, restaurantx, statej, restauranty), domain[statej]))
+				if arc_checker == [False for x in range(len(arc_checker))]:
+					domain[statei].remove(restaurantx)
+					gone = True
+			return gone
+
+	"""****************************************************************************************
+		CODE FOR BACKTRACKING WITH FORWARD CHECK
+	   ****************************************************************************************"""
+	unassigned = copy.deepcopy(states.keys())
+	curr_domains = copy.deepcopy(state_domains)
+	curr_deleted = {}
+	for state in states:
+		curr_deleted[state] = []
+
+	#MAKE THESE USER INPUT SOMEHOW
+	star_weight = starweight
+	reviews_weight = reviewweight
+
+	def stardomain(var, curr_domains):
+		"""
+			Sorts the domains in terms of the user's input weightings
+		"""
+		if curr_domains:
+			stardomain = sorted(curr_domains[state], key = lambda restaurant: (star_weight*math.log10(restaurant['stars']) + reviews_weight*math.log10(restaurant['review_count']))/(star_weight + reviews_weight))
+			stardomain.reverse()
+		else:
+			stardomain = sorted(state_domains[state], key = lambda restaurant: (star_weight*math.log10(restaurant['stars']) + reviews_weight*math.log10(restaurant['review_count']))/(star_weight + reviews_weight))
+			stardomain.reverse()
+		return stardomain
+
+	def backtrack(states, domains, neighbors, user_dict):
+		"""
+			CSP Backtrack
+		"""
+		counter = 0
+		curr_domains = copy.deepcopy(domains)
+		for meal in states:
+			curr_deleted[meal] = []
+		return recurse({}, states, domains, neighbors, user_dict)
+
+	#Sets global, mutable var for recurse function
+	varr = [None]
+	def recurse(assignment, states, domains, neighbors, user_dict):
+		"""
+			Recursive part of backtracking call.
+		"""
+		if len(unassigned) == 0:
+			return assignment
+
+		varr[0] = randomchooseanddelete()
+
+		for val in stardomain(varr[0], curr_domains):
+			assignment[varr[0]] = val
+			forwardcheck(varr[0], val, assignment, user_dict)
+			nextstep = recurse(assignment, states, domains, neighbors, user_dict)
+			if nextstep != None:
+				return nextstep
+		return None
+
+	def forwardcheck(var, val, assignment, user_dict):
+		"""
+			Forward check heuristic to make domains of other variables consistent with
+			the one that has been most recently set
+		"""
+		if curr_domains:
+			for (meal, restaurant) in curr_deleted[var]:
+				curr_domains[meal].append(restaurant)
+			curr_deleted[var] = []
+
+			for meal in neighbors[var]:
+				if meal not in assignment:
+					for restaurant in curr_domains[meal][:]:
+						num_cats = count_categories(assignment.values())
+						if not constraints_match(num_cats, user_dict):
+						#if not user_solution_checker(user_dict, meal, restaurant, assignment):
+							curr_domains[meal].remove(restaurant)
+							curr_deleted[var].append((meal, restaurant))
+
+	def randomchooseanddelete():
+		"""
+			Will randomly choose the next variable to assigne and remove it from the 
+			unassigned list
+		"""
+		var = random.choice(unassigned)
+		unassigned.remove(var)
+		return var
+
+	def fitness (assignment):
+		def func(avgs, avgr) : return (star_weight*math.log10(avgs) + reviews_weight*math.log10(avgr))/(star_weight + reviews_weight)
+		avgstar = 0
+		avgreview = 0
+		for restaurant in assignment.values():
+			l = len(assignment.values())
+			avgstar += restaurant['stars']/l
+			avgreview += restaurant['review_count']/l
+		avgfit = func(avgstar, avgreview)
+		return avgfit
+	
+	solution = backtrack(states, state_domains, neighbors, constraints)
+	total_fitness = fitness(solution)
+	time = time.time() - start
+
+	return (total_fitness , time)
+
+
---
+++ test_cases.py
@@ -0,0 +1,395 @@
+"""THE BASIC BASE CASE FROM WHICH EACH TEST VARIES:
+City: 'Phoenix'
+Constraints: {'Unique': False, 'Mexican': 1, 'Pizza': 1}
+Weights: {'reviews': 2, 'stars': 5}
+num_meals = 6 (days = 2)
+
+Therefore, we will test for different cities, constraints length, uniqueness, weights, and num_meals"""
+
+"""Test Case Category 1 holds everything constant but the location"""
+"""
+TEST CASE 1a
+	"Basic"
+	City: 'Phoenix'
+	Constraints: {'Unique': True}
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 6 
+"""
+# Filter down to restaurants in Phoenix
+city1a = 'Phoenix'
+#rests1 = filter_restaurants(data, 'Phoenix')
+num_meals1a = 6
+constraints1a = {'Unique': False, 'Mexican': 1, 'Pizza': 1}
+weights1a = {'reviews': 2, 'stars': 5}
+test_case1a = (city1a, weights1a['reviews'], weights1a['stars'], num_meals1a, constraints1a)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 1 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 1b
+	"Basic"
+	City: 'Belmont'
+	Constraints: {'Unique': True}
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 6
+"""
+# Filter down to restaurants in Belmont
+city1b = 'Belmont'
+#rests = filter_restaurants(data, 'Belmont')
+num_meals1b = 6
+constraints1b = {'Unique': False, 'Mexican': 1, 'Pizza': 1}
+weights1b = {'reviews': 2, 'stars': 5}
+test_case1b = (city1b, weights1b['reviews'], weights1b['stars'], num_meals1b, constraints1b)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 1 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 1c
+	"Basic"
+	City: 'Parma'
+	Constraints: {'Unique': True}
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 6
+"""
+# Filter down to restaurants in Parma
+city1c = 'Parma'
+#rests = filter_restaurants(data, 'Parma')
+num_meals1c = 6
+constraints1c = {'Unique': False, 'Mexican': 1, 'Pizza': 1}
+weights1c = {'reviews': 2, 'stars': 5}
+test_case1c = (city1c, weights1c['reviews'], weights1c['stars'], num_meals1c, constraints1c)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 1 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""Test Case Category 2 holds everything constant but the number of constraints"""
+"""
+TEST CASE 2
+	"Basic plus a few constraints, some weight on reviews"
+	City: 'Phoenix'
+	Constraints: {'Unique': True, 'Mexican': 0, 'Pizza': 0}
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 6
+"""
+# Filter down to restaurants in Phoenix
+city2 = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals2 = 6
+constraints2 = {'Unique': True, 'Mexican': 1, 'Pizza': 1}
+weights2 = {'reviews': 2, 'stars': 5}
+test_case2 = (city2, weights2['reviews'], weights2['stars'], num_meals2, constraints2)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 2 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 2a
+	"Basic plus a few constraints"
+	City: 'Phoenix'
+	Constraints: {'Unique': True, 'Mexican': 0, 'Pizza': 0}
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 6
+"""
+# Filter down to restaurants in Phoenix
+city2a = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals2a = 6
+constraints2a = {'Unique': True, 'Mexican': 1, 'Pizza': 1}
+weights2a = {'reviews': 2, 'stars': 5}
+test_case2a = (city2a, weights2a['reviews'], weights2a['stars'], num_meals2a, constraints2a)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 2 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 2b
+	"Basic plus a few constraints, some weight on reviews"
+	City: 'Phoenix'
+	Constraints: {'Unique': True, 'Mexican': 0, 'Pizza': 0}
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 6
+"""
+# Filter down to restaurants in Phoenix
+city2b = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals2b = 6
+constraints2b = {'Unique': True, 'Mexican': 0, 'Pizza': 1, 'Chinese': 2}
+weights2b = {'reviews': 2, 'stars': 5}
+test_case2b = (city2b, weights2b['reviews'], weights2b['stars'], num_meals2b, constraints2b)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 2 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 2c
+	"Basic plus a few constraints, some weight on reviews"
+	City: 'Phoenix'
+	Constraints: {'Unique': True, 'Mexican': 0, 'Pizza': 0}
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 6
+"""
+# Filter down to restaurants in Phoenix
+city2c = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals2c = 6
+constraints2c = {'Unique': True, 'Mexican': 1, 'Pizza': 0, 'Chinese': 1, 'Bars': 1, 'Japanese': 2}
+weights2c = {'reviews': 2, 'stars': 5}
+test_case2c = (city2c, weights2c['reviews'], weights2c['stars'], num_meals2c, constraints2c)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 2 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+""" Test Case Category 3 is the same as Test Case Category 2 but with Unique = False"""
+
+"""
+TEST CASE 3a
+	"Basic plus a few constraints"
+	City: 'Phoenix'
+	Constraints: {'Unique': True, 'Mexican': 0, 'Pizza': 0}
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 6
+"""
+# Filter down to restaurants in Phoenix
+city3a = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals3a = 6
+constraints3a = {'Unique': False, 'Mexican': 1, 'Pizza': 1}
+weights3a = {'reviews': 2, 'stars': 5}
+test_case3a = (city3a, weights3a['reviews'], weights3a['stars'], num_meals3a, constraints3a)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 2 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 3b
+	"Basic plus a few constraints, some weight on reviews"
+	City: 'Phoenix'
+	Constraints: {'Unique': True, 'Mexican': 0, 'Pizza': 0}
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 6
+"""
+# Filter down to restaurants in Phoenix
+city3b = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals3b = 6
+constraints3b = {'Unique': False, 'Mexican': 0, 'Pizza': 1, 'Chinese': 2}
+weights3b = {'reviews': 2, 'stars': 5}
+test_case3b = (city3b, weights3b['reviews'], weights3b['stars'], num_meals3b, constraints3b)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 2 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 3c
+	"Basic plus a few constraints, some weight on reviews"
+	City: 'Phoenix'
+	Constraints: {'Unique': True, 'Mexican': 0, 'Pizza': 0}
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 6
+"""
+# Filter down to restaurants in Phoenix
+city3c = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals3c = 6
+constraints3c = {'Unique': False, 'Mexican': 1, 'Pizza': 0, 'Chinese': 1, 'Bars': 1, 'Japanese': 2}
+weights3c = {'reviews': 2, 'stars': 5}
+test_case3c = (city3c, weights3c['reviews'], weights3c['stars'], num_meals3c, constraints3c)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 2 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 4a
+	"Basic"
+	City: 'Phoenix'
+	Constraints: {'Unique': True}
+	Weights: {'reviews': 0, 'stars': 5}
+	num_meals: 6 
+"""
+# Filter down to restaurants in Phoenix
+city4a = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals4a = 6
+constraints4a = {'Unique': False, 'Mexican': 1, 'Pizza': 1}
+weights4a = {'reviews': 0, 'stars': 5}
+test_case4a = (city4a, weights4a['reviews'], weights4a['stars'], num_meals4a, constraints4a)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 1 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 4b
+	"Basic"
+	City: 'Phoenix'
+	Constraints: {'Unique': True}
+	Weights: {'reviews': 3, 'stars': 3}
+	num_meals: 6 
+"""
+# Filter down to restaurants in Phoenix
+city4b = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals4b = 6
+constraints4b = {'Unique': False, 'Mexican': 1, 'Pizza': 1}
+weights4b = {'reviews': 3, 'stars': 3}
+test_case4b = (city4b, weights4b['reviews'], weights4b['stars'], num_meals4b, constraints4b)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 1 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 4c
+	"Basic"
+	City: 'Phoenix'
+	Constraints: {'Unique': True}
+	Weights: {'reviews': 5, 'stars': 0}
+	num_meals: 6 
+"""
+# Filter down to restaurants in Phoenix
+city4c = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals4c = 6
+constraints4c = {'Unique': False, 'Mexican': 1, 'Pizza': 1}
+weights4c = {'reviews': 5, 'stars': 0}
+test_case4c = (city4a, weights4c['reviews'], weights4c['stars'], num_meals4c, constraints4c)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 1 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 5a
+	"Basic"
+	City: 'Phoenix'
+	Constraints: Few
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 10
+"""
+# Filter down to restaurants in Phoenix
+city5a = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals5a = 10
+constraints5a = {'Unique': False, 'Mexican': 1, 'Pizza': 1}
+weights5a = {'reviews': 2, 'stars': 5}
+test_case5a = (city5a, weights5a['reviews'], weights5a['stars'], num_meals5a, constraints5a)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 1 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 5b
+	"Basic"
+	City: 'Phoenix'
+	Constraints: Many
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 50
+"""
+# Filter down to restaurants in Phoenix
+city5b = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals5b = 51
+constraints5b = {'Unique': False, 'Mexican': 1, 'Pizza': 3, 'Sports Bars': 2, 'Japanese': 17, }
+weights5b = {'reviews': 2, 'stars': 5}
+test_case5b = (city5b, weights5b['reviews'], weights5b['stars'], num_meals5b, constraints5b)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 1 OUTPUT:")
+format_meals(output, weights, run_time)"""
+
+"""
+TEST CASE 5c
+	"Basic"
+	City: 'Phoenix'
+	Constraints: Many
+	Weights: {'reviews': 2, 'stars': 5}
+	num_meals: 100
+"""
+# Filter down to restaurants in Phoenix
+city5c = 'Belmont'
+#rests = filter_restaurants(data, 'Phoenix')
+num_meals5c = 99
+constraints5c = {'Unique': False, 'Mexican': 1, 'Pizza': 1, 'Sports Bars': 5, 'Japanese': 10, 'Chinese': 6, 'Juice': 2, 'Pubs': 1, 'Indian': 9, 'Bagels': 3}
+weights5c = {'reviews': 2, 'stars': 5}
+test_case5c = (city5c, weights5c['reviews'], weights5c['stars'], num_meals5c, constraints5c)
+
+"""# Make initial greedy assignment
+curr_rests = make_greedy(rests, num_meals, weights)
+# Get output
+output, run_time = min_conflicts(curr_rests, num_meals, constraints, weights)
+# Print output
+print("TEST CASE 1 OUTPUT:")
+format_meals(output, weights, run_time)"""
